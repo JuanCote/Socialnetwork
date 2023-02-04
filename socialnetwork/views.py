8e08dcb63b496ff6d4eeabca635e2b945a42d1e4
JuanCote <lyy1707@gmail.com>
2023-02-04 14:32:22 +0200

    refactoring friends

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -76,26 +76,26 @@ def logout(request):
 @login_required(login_url='index')
 def friends_all(request):
     users = User.objects.select_related('profile').exclude(username=request.user)
-    subscriptions_id = Friends.objects.filter(user1=request.user).values_list('user2', flat=True)
-    subscriptions = User.objects.filter(username__in=subscriptions_id)
+    subscriptions = Friends.objects.filter(user1=request.user)
+    subscriptions = [x.user2 for x in subscriptions]
     data = {'users': users, 'current_user': request.user, 'subscriptions': subscriptions, 'page': 0}
     return render(request, 'friends_all.html', context=data)
 
 
 @login_required(login_url='index')
 def friends_subscriptions(request):
-    subscriptions_id = Friends.objects.filter(user1=request.user).values_list('user2', flat=True)
-    subscriptions = User.objects.filter(username__in=subscriptions_id)
+    subscriptions = Friends.objects.filter(user1=request.user)
+    subscriptions = [x.user2 for x in subscriptions]
     data = {'subscriptions': subscriptions, 'current_user': request.user, 'page': 2}
     return render(request, 'friends_subscriptions.html', context=data)
 
 
 @login_required(login_url='index')
 def friends_subscribers(request):
-    subscribers_id = Friends.objects.filter(user2=request.user).values_list('user1', flat=True)
-    subscriptions_id = Friends.objects.filter(user1=request.user).values_list('user2', flat=True)
-    subscribers = User.objects.filter(username__in=subscribers_id)
-    subscriptions = User.objects.filter(username__in=subscriptions_id)
+    subscribers = Friends.objects.filter(user2=request.user)
+    subscriptions = Friends.objects.filter(user1=request.user)
+    subscribers = [x.user1 for x in subscribers]
+    subscriptions = [x.user2 for x in subscriptions]
     data = {'subscribers': subscribers, 'subscriptions': subscriptions, 'current_user': request.user, 'page': 1}
     return render(request, 'friends_subscribers.html', context=data)
 
