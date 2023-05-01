1d5cc6517b529199baa0eab8ac0e37b86da9d45a
JuanCote <lyy1707@gmail.com>
2023-05-01 20:00:58 +0300

    add edit profile

--- socialnetwork/forms.py
+++ socialnetwork/forms.py
@@ -4,68 +4,205 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_image_file_extension
 
-from .models import Post
+from .models import Post, Profile
 
 
 class Login(forms.ModelForm):
-    password = forms.CharField(max_length=60, required=True, min_length=6, widget=forms.PasswordInput(
-        attrs={"class": "pl-3 h-12 rounded-md border-slate-500 border mb-3", "placeholder": "Password"}))
+    password = forms.CharField(
+        max_length=60,
+        required=True,
+        min_length=6,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "pl-3 h-12 rounded-md border-slate-500 border mb-3",
+                "placeholder": "Password",
+            }
+        ),
+    )
 
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ["email", "password"]
         widgets = {
-            'email': forms.EmailInput(
-                attrs={"placeholder": "Email adress", "class": "pl-3 h-12 rounded-md border-slate-500 border mb-3"})
+            "email": forms.EmailInput(
+                attrs={
+                    "placeholder": "Email adress",
+                    "class": "pl-3 h-12 rounded-md border-slate-500 border mb-3",
+                }
+            )
         }
 
 
 class Register(forms.ModelForm):
-    first_name = forms.CharField(min_length=2, max_length=30, required=True, widget=forms.TextInput(
-        attrs={"placeholder": "First name", "class": "rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300"}))
-    last_name = forms.CharField(min_length=2, max_length=30, required=True, widget=forms.TextInput(
-        attrs={"class": "bg-slate-100 ml-3 rounded-md pl-3 w-1/2 border-gray-300 border", "placeholder": "Surname"}))
-    password = forms.CharField(min_length=6, max_length=30, required=True, widget=forms.PasswordInput(
-        attrs={"class": "mt-3 h-10 bg-slate-100 w-full rounded-md pl-3 border-gray-300 border",
-               "placeholder": "New password"}))
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "First name",
+                "class": "rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300",
+            }
+        ),
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "bg-slate-100 ml-3 rounded-md pl-3 w-1/2 border-gray-300 border",
+                "placeholder": "Surname",
+            }
+        ),
+    )
+    password = forms.CharField(
+        min_length=6,
+        max_length=30,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "mt-3 h-10 bg-slate-100 w-full rounded-md pl-3 border-gray-300 border",
+                "placeholder": "New password",
+            }
+        ),
+    )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"class": "mt-3 h-10 bg-slate-100 w-full rounded-md pl-3 border-gray-300 border",
-                                       "placeholder": "Email adress"}))
+        widget=forms.EmailInput(
+            attrs={
+                "class": "mt-3 h-10 bg-slate-100 w-full rounded-md pl-3 border-gray-300 border",
+                "placeholder": "Email adress",
+            }
+        )
+    )
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'password', 'email']
+        fields = ["first_name", "last_name", "password", "email"]
 
     def clean_email(self):
-        email = self.cleaned_data['email']
+        email = self.cleaned_data["email"]
         if User.objects.filter(email=email).exists():
-            self.add_error(None, 'Email address already exists')
+            self.add_error(None, "Email address already exists")
         return email
 
     def clean_password(self):
-        return make_password(self.cleaned_data['password'])
+        return make_password(self.cleaned_data["password"])
 
     def clean(self):
         try:
-            last_id = User.objects.latest('id').id
+            last_id = User.objects.latest("id").id
         except:
             last_id = 0
         self.instance.username = last_id + 1
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(max_length=188, widget=forms.Textarea(attrs={"type": "text",
-                                                                           "cols": "80",
-                                                                            "class": "overflow-y-hidden resize-none w-full h-16 p-4 border-b rounded outline-none font-sans font-medium text-base",
-                                                                           "placeholder": "What`s on your mind"}))
-    image = forms.ImageField(required=False, validators=[validate_image_file_extension], widget=forms.FileInput(
-        attrs={
-            'class': 'w-0.5 h-0.5 absolute opacity-0',
-            'id': 'file-input',
-            'onChange': "handleImageUpload()"
-        }
-    ))
+    content = forms.CharField(
+        max_length=188,
+        widget=forms.Textarea(
+            attrs={
+                "type": "text",
+                "cols": "80",
+                "class": "overflow-y-hidden resize-none w-full h-16 p-4 border-b rounded outline-none font-sans font-medium text-base",
+                "placeholder": "What`s on your mind",
+            }
+        ),
+    )
+    image = forms.ImageField(
+        required=False,
+        validators=[validate_image_file_extension],
+        widget=forms.FileInput(
+            attrs={
+                "class": "w-0.5 h-0.5 absolute opacity-0",
+                "id": "file-input",
+                "onChange": "handleImageUpload()",
+            }
+        ),
+    )
 
     class Meta:
         model = Post
-        fields = ['content', 'image']
+        fields = ["content", "image"]
+
+
+class ProfileForm(forms.ModelForm):
+    about_me = forms.CharField(
+        min_length=2,
+        max_length=50,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "About me",
+                "class": "rounded-md bg-slate-100 h-10 w-full pl-3 border border-gray-300",
+            }
+        ),
+    )
+    interested_in = forms.CharField(
+        min_length=2,
+        max_length=50,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Interested in",
+                "class": "rounded-md bg-slate-100 h-10 w-full pl-3 border border-gray-300",
+            }
+        ),
+    )
+    relationship_status = forms.CharField(
+        min_length=2,
+        max_length=23,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Relationship",
+                "class": "rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300",
+            }
+        ),
+    )
+    looking_for = forms.CharField(
+        min_length=2,
+        max_length=23,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Looking for",
+                "class": "rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300",
+            }
+        ),
+    )
+    sex = forms.CharField(
+        min_length=2,
+        max_length=10,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Sex",
+                "class": "rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300",
+            }
+        ),
+    )
+    birthday = forms.DateField(widget=forms.TextInput(attrs={'class': 'rounded-md bg-slate-100 h-10 w-1/2 pl-3 border border-gray-300', 'type':'date'}))
+    hometown = forms.CharField(
+        min_length=2,
+        max_length=23,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Hometown",
+                "class": "rounded-md bg-slate-100 h-10 w-full pl-3 border border-gray-300",
+            }
+        ),
+    )
+
+    class Meta:
+        model = Profile
+        fields = [
+            "sex",
+            "interested_in",
+            "relationship_status",
+            "looking_for",
+            "birthday",
+            "hometown",
+            "about_me",
+        ]
--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -13,43 +13,50 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect
 
-from .forms import Login, Register, PostForm, Post
+from .forms import Login, Register, PostForm, Post, ProfileForm
 from .models import Post, Friends, Profile
 
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect('home_page')
+        return redirect("home_page")
 
     hidden = True
     form_login = Login()
     form_register = Register()
 
-    if request.method == 'POST' and 'Login' in request.POST:
+    if request.method == "POST" and "Login" in request.POST:
         form_login = Login(request.POST)
         if form_login.is_valid():
-            user_from_db = User.objects.filter(email=form_login.cleaned_data['email'])
+            user_from_db = User.objects.filter(email=form_login.cleaned_data["email"])
             if user_from_db:
                 username = user_from_db[0].username
-                user = authenticate(username=username, password=form_login.cleaned_data['password'])
+                user = authenticate(
+                    username=username, password=form_login.cleaned_data["password"]
+                )
                 if user is not None:
                     login(request, user)
-                    return redirect('home_page')
+                    return redirect("home_page")
                 else:
-                    form_login.add_error(None, 'Invalid password')
+                    form_login.add_error(None, "Invalid password")
             else:
-                form_login.add_error(None, 'Invalid email')
-    elif request.method == 'POST' and 'Register' in request.POST:
+                form_login.add_error(None, "Invalid email")
+    elif request.method == "POST" and "Register" in request.POST:
         form_register = Register(request.POST)
         if form_register.is_valid():
             form_register.save()
-            messages.success(request, 'Success registration')
-            data = {'form_login': Login(), 'form_register': form_register, 'hidden': True, messages: messages}
-            return render(request, 'index.html', context=data)
+            messages.success(request, "Success registration")
+            data = {
+                "form_login": Login(),
+                "form_register": form_register,
+                "hidden": True,
+                messages: messages,
+            }
+            return render(request, "index.html", context=data)
         else:
             hidden = False
-    data = {'form_login': form_login, 'form_register': form_register, 'hidden': hidden}
-    return render(request, 'index.html', context=data)
+    data = {"form_login": form_login, "form_register": form_register, "hidden": hidden}
+    return render(request, "index.html", context=data)
 
 
 @receiver(post_save, sender=User)
@@ -58,83 +65,122 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def home_page(request):
     user = request.user
     form = PostForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            post = Post(content=form.cleaned_data['content'], image=form.cleaned_data['image'], user=user)
+            post = Post(
+                content=form.cleaned_data["content"],
+                image=form.cleaned_data["image"],
+                user=user,
+            )
             post.save()
-            return redirect('home_page')
+            return redirect("home_page")
         else:
-            print('not valid')
+            print("not valid")
     else:
-        posts = Post.objects.order_by('-created_at')
-        data = {'form': form, 'posts': posts, 'user': user}
-        return render(request, 'news.html', context=data)
+        posts = Post.objects.order_by("-created_at")
+        data = {"form": form, "posts": posts, "user": user}
+        return render(request, "news.html", context=data)
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def logout(request):
     django_logout(request)
-    return redirect('index')
+    return redirect("index")
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def friends_all(request):
-    users = User.objects.select_related('profile').exclude(username=request.user)
+    users = User.objects.select_related("profile").exclude(username=request.user)
     subscriptions = Friends.objects.filter(user1=request.user)
     subscriptions = [x.user2 for x in subscriptions]
-    data = {'users': users, 'current_user': request.user, 'subscriptions': subscriptions, 'page': 0}
-    return render(request, 'friends_all.html', context=data)
+    data = {
+        "users": users,
+        "current_user": request.user,
+        "subscriptions": subscriptions,
+        "page": 0,
+    }
+    return render(request, "friends_all.html", context=data)
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def friends_subscriptions(request):
     subscriptions = Friends.objects.filter(user1=request.user)
     subscriptions = [x.user2 for x in subscriptions]
-    data = {'subscriptions': subscriptions, 'current_user': request.user, 'page': 2}
-    return render(request, 'friends_subscriptions.html', context=data)
+    data = {"subscriptions": subscriptions, "current_user": request.user, "page": 2}
+    return render(request, "friends_subscriptions.html", context=data)
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def friends_subscribers(request):
     subscribers = Friends.objects.filter(user2=request.user)
     subscriptions = Friends.objects.filter(user1=request.user)
     subscribers = [x.user1 for x in subscribers]
     subscriptions = [x.user2 for x in subscriptions]
-    data = {'subscribers': subscribers, 'subscriptions': subscriptions, 'current_user': request.user, 'page': 1}
-    return render(request, 'friends_subscribers.html', context=data)
+    data = {
+        "subscribers": subscribers,
+        "subscriptions": subscriptions,
+        "current_user": request.user,
+        "page": 1,
+    }
+    return render(request, "friends_subscribers.html", context=data)
 
 
-@login_required(login_url='index')
+@login_required(login_url="index")
 def profile(request):
     user = Profile.objects.get(user_id=request.user)
-    subscribers_subscriptions = Friends.objects.filter(Q(user1=request.user.id) | Q(user2=request.user.id))
-    subscribers, subscriptions = 0, 0
-    for record in subscribers_subscriptions:
-        if record.user1.id == user.user_id:
-            subscriptions += 1
+    form = ProfileForm()
+    form.fields['sex'].initial = user.sex
+    form.fields['interested_in'].initial = user.interested_in
+    form.fields['relationship_status'].initial = user.relationship_status
+    form.fields['looking_for'].initial = user.looking_for
+    form.fields['birthday'].initial = user.birthday
+    form.fields['hometown'].initial = user.hometown
+    form.fields['about_me'].initial = user.about_me
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
         else:
-            subscribers += 1
-    field_names = [str(i).split('.')[-1] for i in user._meta.get_fields()[3:-1]]
-    profile = {'user': user, 'details': []}
-    for field in field_names:
-        if user.__dict__[field]:
-            profile['details'].append(user.__dict__[field])
-        else:
-            profile['details'].append('-')
-    data = {'field_names': field_names, 'profile': profile, 'subscribers': subscribers, 'subscriptions': subscriptions}
-    return render(request, 'profile.html', context=data)
+            print("not valid")
+    else:
+        subscribers_subscriptions = Friends.objects.filter(
+            Q(user1=request.user.id) | Q(user2=request.user.id)
+        )
+        subscribers, subscriptions = 0, 0
+        for record in subscribers_subscriptions:
+            if record.user1.id == user.user_id:
+                subscriptions += 1
+            else:
+                subscribers += 1
+        field_names = [str(i).split(".")[-1] for i in user._meta.get_fields()[3:-1]]
+        profile = {"user": user, "details": []}
+        for field in field_names:
+            if user.__dict__[field]:
+                profile["details"].append(user.__dict__[field])
+            else:
+                profile["details"].append("-")
+        data = {
+            "field_names": field_names,
+            "profile": profile,
+            "subscribers": subscribers,
+            "subscriptions": subscriptions,
+            "form": form,
+            "hidden": True,
+        }
+        return render(request, "profile.html", context=data)
 
 
 def subscribe(request):
     data = json.loads(request.body)
-    users = User.objects.filter(username__in=(data['user1'], data['user2']))
-    if int(users[0].username) == data['user1']:
-        print('shot')
+    users = User.objects.filter(username__in=(data["user1"], data["user2"]))
+    if int(users[0].username) == data["user1"]:
+        print("shot")
         user1, user2 = users[0], users[1]
     else:
         user1, user2 = users[1], users[0]
@@ -144,4 +190,4 @@ def subscribe(request):
     else:
         subscribe = Friends.objects.create(user1=user1, user2=user2)
         subscribe.save()
-    return JsonResponse({'Nikita': 'viktor'})
+    return JsonResponse({"Nikita": "viktor"})
