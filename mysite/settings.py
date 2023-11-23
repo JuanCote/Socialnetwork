41d81a0cb11f80934dbcb421b542c301a88b32e2
JuanCote <lyy1707@gmail.com>
2023-11-23 12:32:47 +0200

    add settings for server and https

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w))@t$#q%iyu24kosyd7eoad3$r7s_*p^q-!u4ua_&%f30*fnx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django.localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django.localhost', 'socialnetwork.bondarenkonikita.pp.ua']
 
 
 # Application definition
@@ -131,3 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_COOKIE_SECURE = True
